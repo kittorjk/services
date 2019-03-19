64c0733a457ab3099b5ab0d8fb5dbc2a8362a107
kittorjk <nestor.rrc@gmail.com>
2019-03-19 00:25:39 -0400

    Migración para agregar campos a employees

---
+++ database/migrations/2019_03_19_025829_alter_employees_add_amounts.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterEmployeesAddAmounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('employees', function (Blueprint $table) {
+        $table->string('category')->after('role');
+        $table->decimal('basic_income',8,2)->after('income');
+        $table->decimal('production_bonus',8,2)->after('basic_income');
+        $table->decimal('payable_amount',8,2)->after('production_bonus');
+        $table->dateTime('date_in_employee')->after('date_in');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('employees', function (Blueprint $table) {
+        $table->dropColumn('category');
+        $table->dropColumn('basic_income');
+        $table->dropColumn('production_bonus');
+        $table->dropColumn('payable_amount');
+        $table->dropColumn('date_in_employee');
+      });
+    }
+}
