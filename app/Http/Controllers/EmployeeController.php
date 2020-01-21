bcdfad4e8a7f2a5c2b7170e1a229a1d2851f7327
Nestor Romero <nestorrrc@wireless-10-105-115-120.umd.edu>
2020-01-21 18:56:47 -0500

    Metodos para creación de usuario desde empleado

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -11,10 +11,11 @@ use View;
 use Mail;
 use Input;
 use Exception;
+use App\Branch;
+use App\Email;
 use App\Employee;
 use App\User;
-use App\Email;
-use App\Branch;
+use App\UserAction;
 use Carbon\Carbon;
 use Hash;
 
@@ -373,5 +374,125 @@ class EmployeeController extends Controller
             return redirect(Session::get('url'));
         else
             return redirect()->route('employee.index');
+    }
+
+    public function add_account($id) {
+      $user = Session::get('user');
+      if ((is_null($user)) || (!$user->id))
+        return redirect()->route('root');
+
+      $service = Session::get('service');
+
+      $employee = Employee::find($id);
+      
+      if (!$employee->corp_email && !$employee->ext_email) {
+        Session::flash('message', 'El registro de empleado debe incluir un correo electrónico corporativo o externo!');
+        return redirect()->back();
+      }
+
+      $add_user = new User();
+
+      $random_string = $this->generatePass();      
+
+      $add_user->name = $employee->first_name.' '.$employee->last_name;
+      $add_user->full_name = $employee->first_name.' '.$employee->last_name;
+      $add_user->login = $random_string;
+      $add_user->password = $random_string;
+
+      $add_user->branch_id = $employee->branch_id;
+      $add_user->branch = $employee->branch;
+      $add_user->area = $employee->area;
+      $add_user->phone = $employee->phone;
+      $add_user->email = $employee->corp_email ?: $employee->ext_email;
+
+      $add_user->priv_level = 0;
+      $add_user->acc_project = 1;
+      $add_user->acc_active = 1;
+      $add_user->acc_warehouse = 0;
+      $add_user->acc_staff = 0;
+
+      $pass_to_hash = $add_user->password;
+      $add_user->password = Hash::make($pass_to_hash);
+
+      // $add_user->cost_day = $add_user->cost > 0 ? $add_user->cost/22 : 0;
+
+      $add_user->status = 'Activo';
+        
+      $add_user->save();
+
+      // Update employee record with user id
+      $employee->access_id = $add_user->id;
+      $employee->save();
+
+      /* Add actions registry */
+      $this->add_actions_registry($add_user->id);
+
+      /* send email */
+      $recipient = $add_user;
+
+      $data = array('recipient' => $recipient);
+      $mail_structure = 'emails.new_user';
+      $subject = 'Nueva cuenta en el sistema gerteabros.com';
+
+      $view = View::make($mail_structure, $data /*['recipient' => $recipient]*/);
+      $content = (string) $view;
+        
+      $success = 1;
+      try {
+        Mail::send($mail_structure, $data, function($message) use($recipient, $subject) {
+                $message->to($recipient->email, $recipient->name)
+                    ->subject($subject)
+                    ->from('postmaster@gerteabros.com', 'postmaster@gerteabros.com');
+        });
+      } catch (Exception $ex) {
+        $success = 0;
+      }
+
+      $email = new Email;
+      $email->sent_by = 'postmaster@gerteabros.com';
+      $email->sent_to = $recipient->email;
+      $email->subject = $subject;
+      $email->content = $content;
+      $email->success = $success;
+      $email->save();
+
+      Session::flash('message', "El usuario fue agregado al sistema de forma correcta!");
+      
+      if (Session::has('url'))
+        return redirect(Session::get('url'));
+      else
+        return redirect()->route('employee.index');
+    }
+
+    public function generatePass()
+    {
+        //A string with the possible characters is defined
+        $chain = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        //The string's length
+        $chainLength = strlen($chain);
+
+        //new variable to store the pass
+        $pass = "";
+        //a variable to define the length of the pass
+        $passLength = 8;
+
+        for ($i=1 ; $i <= $passLength ; $i++) {
+            //generate a random number indicating the position of the character on the string from 0 to length-1
+            $pos = rand(0,$chainLength-1);
+
+            //put the character obtained on the pass variable
+            $pass .= substr($chain,$pos,1);
+        }
+
+        return $pass;
+    }
+
+    function add_actions_registry($id)
+    {
+        //$data = 0; // Every action is restricted for new users
+        $action = new UserAction();
+        $action->user_id = $id;
+
+        $action->save();
     }
 }
